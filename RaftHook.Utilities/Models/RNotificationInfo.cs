4b705662fc1035296da11f476c745038de4f7a18
Revolution <tavinho.daniel@gmail.com>
2023-10-11 12:06:55 -0300

    feat: add custom raft notification

---
+++ RaftHook.Utilities/Models/RNotificationInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace RaftHook.Utilities.Models
+{
+    public class RNotificationInfo : Notification_QuestItem_Info
+    {
+        public RNotificationInfo(string description) : base(null, 0, null)
+        {
+            itemSprite = RaftClient.CheatLogo;
+            itemName = description;
+        }
+    }
+}
--- RaftHook.Utilities/RaftClient.cs
+++ RaftHook.Utilities/RaftClient.cs
@@ -1,10 +1,14 @@
-﻿namespace RaftHook.Utilities
+﻿using RaftHook.Utilities.Models;
+using UnityEngine;
+
+namespace RaftHook.Utilities
 {
     public static class RaftClient
     {
         public static Network_Player LocalPlayer => SingletonGeneric<Network_Entity>.Singleton.Network.GetLocalPlayer();
         public static bool IsInLobby => LoadSceneManager.IsLoadingLobbyScene;
         public static bool IsInGame => LoadSceneManager.IsGameSceneLoaded;
+        public static Sprite CheatLogo => GetItemSprite("Placeable_Anchor_Stationary_Advanced");
 
         public static void GiveItem(string itemName, int amount = 1)
         {
@@ -12,6 +16,21 @@
             if (item == null) return;
 
             LocalPlayer.Inventory.AddItem(item.UniqueName, amount);
+        }
+
+        public static void SendNotification(string message)
+        {
+            var notificationManager = ComponentManager<NotificationManager>.Value;
+            if (notificationManager == null) return;
+
+            (notificationManager.ShowNotification("QuestItem") as Notification_QuestItem)
+                ?.infoQue.Enqueue(new RNotificationInfo(message));
+        }
+
+        private static Sprite GetItemSprite(string itemName)
+        {
+            var item = ItemManager.GetItemByName(itemName);
+            return item.settings_Inventory?.Sprite;
         }
     }
 }
