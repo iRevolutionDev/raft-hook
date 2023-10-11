ca88134c499b6877f381eedf0eef854a76ccd278
Revolution <tavinho.daniel@gmail.com>
2023-10-11 12:07:22 -0300

    feat: add PrivateValueAccessor for access private fields

---
+++ RaftHook.Utilities/PrivateValueAccessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace RaftHook.Utilities
+{
+    public class PrivateValueAccessor
+    {
+        private const BindingFlags Flags = BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.SetProperty |
+                                           BindingFlags.GetField | BindingFlags.SetField | BindingFlags.NonPublic;
+
+        public static PropertyInfo GetPrivatePropertyInfo(Type type, string propertyName)
+        {
+            var props = type.GetProperties(Flags);
+            return props.FirstOrDefault(propInfo => propInfo.Name == propertyName);
+        }
+
+        public static FieldInfo GetPrivateFieldInfo(Type type, string fieldName)
+        {
+            var fields = type.GetFields(Flags);
+            return fields.FirstOrDefault(fieldInfo => fieldInfo.Name == fieldName);
+        }
+
+        public static object GetPrivateFieldValue(Type type, string fieldName, object o)
+        {
+            return GetPrivateFieldInfo(type, fieldName).GetValue(o);
+        }
+    }
+}
