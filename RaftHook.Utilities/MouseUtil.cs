b0886368f773b433078d69beb1dbdc0ba4d56c90
Revolution <tavinho.daniel@gmail.com>
2023-10-11 10:47:52 -0300

    fix: disable input on open menus

--- RaftHook.Utilities/MouseUtil.cs
+++ RaftHook.Utilities/MouseUtil.cs
@@ -4,11 +4,11 @@ namespace RaftHook.Utilities
 {
     public abstract class MouseUtil
     {
-        public static void ToggleCursor(bool state, bool lockMouseLook)
+        private static void ToggleCursor(bool state, bool lockMouseLook)
         {
-            Helper.SetCursorLockState(state ? CursorLockMode.Confined : CursorLockMode.Locked);
-            Helper.SetCursorVisible(state);
+            Helper.SetCursorVisibleAndLockState(state, lockMouseLook ? CursorLockMode.None : CursorLockMode.Locked);
             ComponentManager<Network_Player>.Value.PlayerScript.SetLockMouseLook(lockMouseLook);
+            CanvasHelper.ActiveMenu = state ? MenuType.PauseMenu : MenuType.None;
         }
 
         public static void ToggleCursor(bool state)
