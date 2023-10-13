2ec074b994f70db499b70cba38c9cf6bd19fc42a
Revolution <tavinho.daniel@gmail.com>
2023-10-13 19:39:40 -0300

    refactor: cheat spoofing and extend functionality

--- RaftHook.Features/Features/Miscellaneous/SpoofDevCheats.cs
+++ RaftHook.Features/Features/Miscellaneous/SpoofDevCheats.cs
@@ -5,12 +5,34 @@ using RaftHook.Utilities;
 namespace RaftHook.Features.Features.Miscellaneous
 {
     [HarmonyPatch(typeof(Cheat), "AllowCheatsForLocalPlayer")]
-    internal static class SpoofDevCheats
+    internal static class AllowCheatsForLocalPlayerPatch
     {
-        private static bool Prefix()
+        private static bool Postfix(bool __result)
         {
-            if (RaftSettings.SpoofDevCheats) MelonLogger.Msg("Spoofed dev cheats");
-            return !RaftSettings.SpoofDevCheats;
+            Cheat.UseCheats = RaftSettings.SpoofDevCheats;
+            if (RaftSettings.SpoofDevCheats) MelonLogger.Msg("[AllowCheatsForLocalPlayerPatch] Spoofed dev cheats");
+            return RaftSettings.SpoofDevCheats;
+        }
+    }
+
+    [HarmonyPatch(typeof(Cheat), "AllowCheatsForUser")]
+    internal static class AllowCheatsForUserPatch
+    {
+        private static bool Postfix(bool __result)
+        {
+            Cheat.UseCheats = RaftSettings.SpoofDevCheats;
+            if (RaftSettings.SpoofDevCheats) MelonLogger.Msg("[AllowCheatsForUserPatch] Spoofed dev cheats");
+            return RaftSettings.SpoofDevCheats;
+        }
+    }
+
+    [HarmonyPatch(typeof(Cheat), "IsUserDev")]
+    internal static class IsUserDevPatch
+    {
+        private static bool Postfix(bool __result)
+        {
+            if (RaftSettings.SpoofDevCheats) MelonLogger.Msg("[IsUserDevPatch] Spoofed dev cheats");
+            return RaftSettings.SpoofDevCheats;
         }
     }
 }
